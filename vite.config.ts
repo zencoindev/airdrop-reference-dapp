99466caf1341bb5932d1b3a79f95f01d4a13df7c
Max Voloshinskii <me@voloshinskii.ru>
2025-01-21 17:19:04 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [nodePolyfills(),react()],
-  base: "/"
+  base: "/airdrop-reference-dapp/"
 })
