ea890a0f0e12e8e2fda536f53df97e644fb63190
Max Voloshinskii <me@voloshinskii.ru>
2025-01-21 19:03:08 +0300

    Remove tonapi key

--- src/tonapi.ts
+++ src/tonapi.ts
@@ -2,7 +2,6 @@ import { TonApiClient } from "@ton-api/client";
 
 const ta = new TonApiClient({
   baseUrl: "https://tonapi.io",
-  apiKey: import.meta.env.VITE_TONAPI_KEY || undefined,
 });
 
 export default ta;
