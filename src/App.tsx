e974bc345716f6725746fe2f6fcd5495f29c3b13
Moiseev Ilya <ilya@mois.pro>
2025-01-22 15:54:55 +0100

    feat: add TonConnectButton to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,153 +1,198 @@
-import {useCallback, useEffect, useMemo, useState} from "react";
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { useTonAddress, useTonConnectUI } from "@tonconnect/ui-react";
-import { Address } from "@ton/core";
+import { useTonAddress, useTonConnectUI } from '@tonconnect/ui-react';
+import { Address } from '@ton/core';
 
-import "./App.css";
-import { List } from "./components/List/List";
-import { Loader } from "./components/Loader/Loader";
-import { claimAPI } from "./api";
-import { UserClaim } from "./api/claimAPI";
-import { isValidAddress } from "./utils/address";
-import { showConfetti } from "./utils/confetti";
-import tonapi from "./tonapi";
-import { JettonInfo } from "@ton-api/client";
-import { fromNano } from "./utils/decimals";
+import './App.css';
+import { List } from './components/List/List';
+import { Loader } from './components/Loader/Loader';
+import { claimAPI } from './api';
+import { UserClaim } from './api/claimAPI';
+import { isValidAddress } from './utils/address';
+import { showConfetti } from './utils/confetti';
+import tonapi from './tonapi';
+import { JettonInfo } from '@ton-api/client';
+import { fromNano } from './utils/decimals';
 
 BigInt.prototype.toJSON = function () {
-  return this.toString();
+    return this.toString();
 };
 
 function useQuery() {
-  return useMemo(() => new URLSearchParams(window.location.search), [window.location.search]);
+    return useMemo(
+        () => new URLSearchParams(window.location.search),
+        [window.location.search]
+    );
 }
 
 function App() {
-  const query = useQuery();
-  const [isClaimStatusLoading, setClaimStatusLoading] = useState(true);
-  const [userClaim, setUserClaim] = useState<UserClaim | null>(null);
-  const [jettonInfo, setJettonInfo] = useState<JettonInfo | null>(null);
-  const [isClaiming, setIsClaiming] = useState(false);
+    const query = useQuery();
+    const [isClaimStatusLoading, setClaimStatusLoading] = useState(true);
+    const [userClaim, setUserClaim] = useState<UserClaim | null>(null);
+    const [jettonInfo, setJettonInfo] = useState<JettonInfo | null>(null);
+    const [isClaiming, setIsClaiming] = useState(false);
 
-  const [tonConnectUI] = useTonConnectUI();
+    const [tonConnectUI] = useTonConnectUI();
 
-  const claimId = query.get("claimId") ?? import.meta.env.VITE_CLAIM_UUID;
-  const claimJettonAddress = query.get("claimJetton") ?? import.meta.env.VITE_CLAIM_TOKEN_ADDRESS;
+    const claimId = query.get('claimId') ?? import.meta.env.VITE_CLAIM_UUID;
+    const claimJettonAddress =
+        query.get('claimJetton') ?? import.meta.env.VITE_CLAIM_TOKEN_ADDRESS;
 
+    const connectedAddressString = useTonAddress();
+    const connectedAddress = useMemo(() => {
+        return isValidAddress(connectedAddressString)
+            ? Address.parse(connectedAddressString)
+            : null;
+    }, [connectedAddressString]);
 
-  const connectedAddressString = useTonAddress();
-  const connectedAddress = useMemo(() => {
-    return isValidAddress(connectedAddressString)
-      ? Address.parse(connectedAddressString)
-      : null;
-  }, [connectedAddressString]);
+    useEffect(() => {
+        setTimeout(() => {
+            tonConnectUI.connectionRestored.then((connected) => {
+                if (!connected) {
+                    tonConnectUI.openModal();
+                }
+            });
+        }, 400);
+    }, [tonConnectUI]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      tonConnectUI.connectionRestored.then((connected) => {
-        if (!connected) {
-          tonConnectUI.openModal();
+    useEffect(() => {
+        async function loadJettonInfo() {
+            const storageKey = `jetton_${claimJettonAddress}`;
+            const storedInfo = localStorage.getItem(storageKey);
+            if (storedInfo) {
+                setJettonInfo(JSON.parse(storedInfo));
+                return;
+            }
+            const jettonInfo = await tonapi.jettons.getJettonInfo(
+                Address.parse(claimJettonAddress)
+            );
+            setJettonInfo(jettonInfo);
+            localStorage.setItem(storageKey, JSON.stringify(jettonInfo));
         }
-      });
-    }, 400);
-  }, [tonConnectUI]);
 
-  useEffect(() => {
-    async function loadJettonInfo() {
-      const storageKey = `jetton_${claimJettonAddress}`;
-      const storedInfo = localStorage.getItem(storageKey);
-      if (storedInfo) {
-        setJettonInfo(JSON.parse(storedInfo));
-        return;
-      }
-      const jettonInfo = await tonapi.jettons.getJettonInfo(Address.parse(claimJettonAddress));
-      setJettonInfo(jettonInfo);
-      localStorage.setItem(storageKey, JSON.stringify(jettonInfo));
-    }
+        loadJettonInfo();
+    }, []);
 
-    loadJettonInfo();
-  }, []);
-
-  useEffect(() => {
-    if (connectedAddress) {
-      claimAPI.airdrop.getUserClaim({ account: connectedAddress.toRawString(), id: claimId }).then(userClaim => {
-        setClaimStatusLoading(false);
-        setUserClaim(userClaim);
-      }).catch(() => {
-        setClaimStatusLoading(false);
-        setUserClaim(null);
-      })
-    }
-  }, [connectedAddress]);
-
-  const handleSendMessage = useCallback(() => {
-    const claimMessage = userClaim?.claim_message;
-    if (!claimMessage) {
-      return;
-    }
-    tonConnectUI.sendTransaction({
-      validUntil: Math.floor(Date.now() / 1000) + 300,
-      messages: [
-        {
-          address: claimMessage.address,
-          amount: claimMessage.amount,
-          payload: claimMessage.payload,
-          stateInit: claimMessage.state_init,
+    useEffect(() => {
+        if (connectedAddress) {
+            claimAPI.airdrop
+                .getUserClaim({
+                    account: connectedAddress.toRawString(),
+                    id: claimId,
+                })
+                .then((userClaim) => {
+                    setClaimStatusLoading(false);
+                    setUserClaim(userClaim);
+                })
+                .catch(() => {
+                    setClaimStatusLoading(false);
+                    setUserClaim(null);
+                });
+        } else {
+            setClaimStatusLoading(false);
+            setUserClaim(null);
         }
-      ],
-    }).then(() => {
-      setIsClaiming(true);
-      showConfetti();
-    });
+    }, [connectedAddress]);
 
-  }, [tonConnectUI, userClaim?.claim_message]);
+    const handleSendMessage = useCallback(() => {
+        const claimMessage = userClaim?.claim_message;
+        if (!claimMessage) {
+            return;
+        }
+        tonConnectUI
+            .sendTransaction({
+                validUntil: Math.floor(Date.now() / 1000) + 300,
+                messages: [
+                    {
+                        address: claimMessage.address,
+                        amount: claimMessage.amount,
+                        payload: claimMessage.payload,
+                        stateInit: claimMessage.state_init,
+                    },
+                ],
+            })
+            .then(() => {
+                setIsClaiming(true);
+                showConfetti();
+            });
+    }, [tonConnectUI, userClaim?.claim_message]);
 
-  if (!claimId || !claimJettonAddress) {
+    if (!claimId || !claimJettonAddress) {
+        return (
+            <div className="placeholder">
+                <div className="body1 secondary">
+                    Claim ID or Jetton address is not provided.
+                </div>
+            </div>
+        );
+    }
+
+    if (isClaimStatusLoading) {
+        return (
+            <div className="placeholder">
+                <Loader />
+            </div>
+        );
+    }
+
+    if (!connectedAddress) {
+        return (
+            <div className="placeholder">
+                <div className="body1 secondary">
+                    Please connect your wallet to claim the token.
+                </div>
+            </div>
+        );
+    }
+
+    if (!userClaim?.amount) {
+        return (
+            <div className="placeholder">
+                <div className="body1 secondary">
+                    You have no tokens available for claim.
+                </div>
+            </div>
+        );
+    }
+
     return (
-      <div className="placeholder">
-        <div className="body1 secondary">Claim ID or Jetton address is not provided.</div>
-      </div>
-    )
-  }
-
-  if (isClaimStatusLoading) {
-    return (
-      <div className="placeholder">
-        <Loader />
-      </div>
-    );
-  }
-
-  if (!userClaim?.amount) {
-    return (
-      <div className="placeholder">
-        <div className="body1 secondary">You have no tokens available for claim.</div>
-      </div>
-    );
-  }
-
-  return (
-    <div className={"mw60"}>
-      <div className={"padding-top"} />
-      {jettonInfo && <img alt={jettonInfo.metadata.name} className="jetton-logo" src={jettonInfo.preview} />}
-      <div className="desc-container mb32">
-        <h2>Claim your token</h2>
-      </div>
-      <List>
-        <List.Item title={"Amount"}>
-          {fromNano(userClaim.amount, jettonInfo?.metadata.decimals ?? 9)} {jettonInfo?.metadata.symbol}
-        </List.Item>
-        <List.Item title={"To be paid"}>
-          ~{fromNano(userClaim.claim_message.amount)} TON
-        </List.Item>
-      </List>
-      <div className="end-page">
-        <div className="mw60">
-          <button disabled={isClaiming} className="button primary" onClick={handleSendMessage}><div className="label1">Claim!</div></button>
+        <div className={'mw60'}>
+            <div className={'padding-top'} />
+            {jettonInfo && (
+                <img
+                    alt={jettonInfo.metadata.name}
+                    className="jetton-logo"
+                    src={jettonInfo.preview}
+                />
+            )}
+            <div className="desc-container mb32">
+                <h2>Claim your token</h2>
+            </div>
+            <List>
+                <List.Item title={'Amount'}>
+                    {fromNano(
+                        userClaim.amount,
+                        jettonInfo?.metadata.decimals ?? 9
+                    )}{' '}
+                    {jettonInfo?.metadata.symbol}
+                </List.Item>
+                <List.Item title={'To be paid'}>
+                    ~{fromNano(userClaim.claim_message.amount)} TON
+                </List.Item>
+            </List>
+            <div className="end-page">
+                <div className="mw60">
+                    <button
+                        disabled={isClaiming}
+                        className="button primary"
+                        onClick={handleSendMessage}
+                    >
+                        <div className="label1">Claim!</div>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <TonConnectUIProvider
-      manifestUrl="https://not-burn.tonkeeper.com/manifest.json"
-    >
-      <App />
-    </TonConnectUIProvider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <TonConnectUIProvider manifestUrl="https://not-burn.tonkeeper.com/manifest.json">
+            <TonConnectButton className="tonconnectButton" />
+            <App />
+        </TonConnectUIProvider>
+    </StrictMode>
+);
