fe48f5bdcf83ba0534b42424a8afe1ca82deadfb
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:07:04 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { execSync } from 'child_process'
 
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
