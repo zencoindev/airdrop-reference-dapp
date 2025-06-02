18e9381ae5d9b3c28478786452483d15b8fa8d7e
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:17:29 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { execSync } from 'child_process'
-
-const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
 const base = '/airdrop-reference-dapp/';
 
