ddfbb55297d91bfb258eaba3950f6e3eb2fffa1e
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:08:17 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { execSync } from 'child_process'
 
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
@@ -7,11 +8,8 @@ const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 const base = branch === 'master' ? '/airdrop-reference-dapp/' : `/airdrop-reference-dapp/${branch}/`
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  // это важно, чтобы Vercel не ломал роутинг
+  plugins: [nodePolyfills(), react()],
+  base,
   server: {
     fs: {
       strict: false,
