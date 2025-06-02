6528f88abc272400d9f1cd8b81795a49887a238d
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:25:41 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-const branch = 'main';
-
-const base = branch === 'main' ? '/airdrop-reference-dapp/' : `/airdrop-reference-dapp/${branch}/`
-
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
   server: {
