ed1c409dd4f86d362792970f35b0baa2efef26ac
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:10:14 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { execSync } from 'child_process'
 
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
-const base = branch === 'master' ? '/airdrop-reference-dapp/' : `/airdrop-reference-dapp/${branch}/`
+const base = branch === 'main' ? '/airdrop-reference-dapp/' : `/airdrop-reference-dapp/${branch}/`
 
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
