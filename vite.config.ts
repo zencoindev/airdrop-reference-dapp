eecd33594c882eb5d4d6f894db55a1d22a3ffc9e
Nikolay Shapovalov <nikolay@samegrid.io>
2025-06-03 00:06:27 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 const base = branch === 'master' ? '/airdrop-reference-dapp/' : `/airdrop-reference-dapp/${branch}/`
 
 export default defineConfig({
-  plugins: [nodePolyfills(), react()],
-  base
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  // это важно, чтобы Vercel не ломал роутинг
+  server: {
+    fs: {
+      strict: false,
+    },
+  }
 })
